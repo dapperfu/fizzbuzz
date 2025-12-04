1b21064a03e9d2a0a281bedc630991ae8b455220
Jed <jed@mydomain.local>
2025-12-04 12:40:54 -0500

    Added sanity check.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -5,8 +5,8 @@
 for i in range(1,101):
 	print(i+1)
 	if i%3==0:
-		print(f"{i}: Fizz")
+		print(f"{i}: Fizz i/3={i/3}")
 	if i%5==0:
-		print(f"{i}: Buzz")
+		print(f"{i}: Buzz i/5={i/5}")
 	if i%15==0:
-		print(f"{i}: FizzBuzz")
+		print(f"{i}: FizzBuzz i=15/{i/15}")
