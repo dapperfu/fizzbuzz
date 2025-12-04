29cb7d438411493679a7991e6850bb38c1264512
Jed <jed@mydomain.local>
2025-12-04 12:35:56 -0500

    Logic complete.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 
-print("Hello World")
+#print("Hello World")
 
 for i in range(0,100):
-	print(i+1)
+	#print(i+1)
+	if i%3:
+		print("3")
+	if i%5:
+		print("5")
+	if i%15:
+		print("15")
